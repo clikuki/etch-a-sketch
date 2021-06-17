b66c762a4d4c9ff4c9f5ec71dfd971a0ab9ca5a4
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-06-17 16:44:39 +0800

    link to github account, rainbow brush added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const canvasContainer = document.querySelector('#canvas');
+const changeCanvasSizeBtn = document.querySelector('#changeCanvasSize');
+const switchColorBtn = document.querySelector('#switchColor');
 
-function createCanvas(sideLength) {
+let rainbow = false;
+
+function initialize() {
+	setCanvas(15);
+	addEventListeners();
+}
+
+function setCanvas(sideLength) {
 	let canvasSize = getCanvasSize(sideLength);
 
 	canvasContainer.style.cssText = `width: ${canvasSize}px; height: ${canvasSize}px;`;
@@ -32,8 +41,33 @@ function getCanvasSize(sideLength) {
 	return canvasSize;
 }
 
+function randInt(min, max)  {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function changeSquareColor(square) {
-	square.classList.toggle('hovered')
+	if(rainbow) {
+		if(square.classList.contains('hovered')) {
+			if(square.style.opacity >= .1) {
+				square.style.opacity = square.style.opacity - .1;
+			}
+		}else {
+			const r = randInt(1, 255);
+			const g = randInt(1, 255);
+			const b = randInt(1, 255);
+		
+			square.style.cssText += `background-color: rgb(${r}, ${g}, ${b}); opacity: 1;`;
+			square.classList.remove('black');
+			square.classList.add('hovered');
+		}
+	}else {
+		square.classList.remove('hovered');
+		square.classList.toggle('black');
+		square.style['background-color'] = 'none';
+	}
 }
 
 function removeCanvas() {
@@ -43,4 +77,22 @@ function removeCanvas() {
 	}
 }
 
-createCanvas(15);
+function addEventListeners() {
+	changeCanvasSizeBtn.addEventListener('click', () => {
+		let newSize = +prompt('Enter a new size for the canvas', 16)
+
+		if(isNaN(newSize)) return alert('Invalid input: Must be a number!');
+		if(newSize > 100) newSize = 100;
+		if(newSize < 1) newSize = 1;
+
+		removeCanvas();
+		setCanvas(newSize);
+	});
+
+	switchColorBtn.addEventListener('click', () => {
+		rainbow = !rainbow;
+		switchColorBtn.textContent = `Rainbow paintbrush : ${rainbow}`
+	})
+}
+
+initialize();
